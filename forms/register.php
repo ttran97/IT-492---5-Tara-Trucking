1dc5113029cdd14bfb3f42a1ba4baf565922e853
ttran97 <tonyt2000@outlook.com>
2023-03-14 17:04:12 -0400

    update 3142023 v2

--- forms/register.php
+++ forms/register.php
@@ -1,44 +1,34 @@
 <?php
-// Get the form data
-$name = $_POST['name'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$confirm_password = $_POST['confirm_password'];
-
-// Validate the form data
-if (empty($name) || empty($email) || empty($password) || empty($confirm_password)) {
-  die('Please fill all required fields.');
-}
-
-if ($password != $confirm_password) {
-  die('Passwords do not match. Please try again.');
-}
-
-// Hash the password
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-// Connect to the SQL database
+// Database connection parameters
 $servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "employees";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
 
+// Create a connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check the connection
+// Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
-// Insert the data into the SQL database
-$sql = "INSERT INTO employees (name, email, password) VALUES ('$name', '$email', '$hashed_password')";
+// Get submitted form data
+$name = $_POST["name"]; // Replace "name" with the actual name attribute of your input field
+$email = $_POST["email"]; // Replace "email" with the actual name attribute of your input field
+// Add other input fields as necessary
+
+// Insert the employee information into the database
+$sql = "INSERT INTO employees (name, email) VALUES ('$name', '$email')"; // Replace "employees" with the actual table name in your database
 
 if ($conn->query($sql) === TRUE) {
-  echo "Employee registration successful!";
+    // Redirect to the employee success page after successful registration
+    header("Location: employee-success.html");
+    exit();
 } else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-// Close the connection to the SQL database
+// Close the connection
 $conn->close();
 ?>
